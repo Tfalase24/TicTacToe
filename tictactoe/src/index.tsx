7e436b543b02230b5bdfd19eacaaa9bc94cb1aea
TF <tfalase24@gmail.com>
2023-03-07 00:01:04 +0000

    feat(tictactoe\src\index.tsx): Converted index file to .tsx file

---
+++ tictactoe/src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function App() {
+  return element;
+}
+
+export default App
+
+const element: JSX.Element = (
+  <div>
+  
+  </div>
+);
